763ca4f7adc7901b4467803965b83c5df6704497
Luiz Miguel <luiz.miguel.lmfr30@gmail.com>
2025-06-13 16:58:07 -0300

    navegacao pronta

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleSenha(iconElement) {
+    const input = iconElement.previousElementSibling;
+    const isVisible = input.type === "text";
+    input.type = isVisible ? "password" : "text";
+    iconElement.style.fill = isVisible ? "#555" : "#1e88e5";
+}
+function salvarDadosPesquisa(event, inputId) {
+    event.preventDefault();
+    const valorPesquisa = document.getElementById(inputId).value;
+    if (!valorPesquisa) {
+        return;
+    }
+    sessionStorage.setItem('pesquisa', valorPesquisa);
+    window.location = 'resultado_de_pesquisa.html';
+}
+function pesquisarTag(element) {
+    const valor = element.textContent;
+    sessionStorage.setItem('pesquisa', valor);
+    window.location = 'resultado_de_pesquisa.html';
+}
+function verTrabalho(element) {
+    const valor = element.textContent;
+    sessionStorage.setItem('pesquisa', valor);
+    window.location = 'visualizacao_de_vaga.html';
+}
